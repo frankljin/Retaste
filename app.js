d6d5603b0a517ebc56f65d78c5d226c078b9ad5d
frankljin <52306663+frankljin@users.noreply.github.com>
2020-12-18 15:57:55 -0700

    ejs

--- app.js
+++ app.js
@@ -4,16 +4,19 @@ const request = require("request");
 const api_url = "https://api.spoonacular.com/recipes/findByIngredients";
 const api_key = "5ebf32f8374945ddba4efdce210b90d3";
 const app = express();
+const ejs = require("ejs");
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.set('view engine', 'ejs');
+
 app.get("/", function(req, res){
-    res.sendFile(__dirname + "/index.html");
+    res.render("home");
 });
 
 app.get("/results", function(req, res){
-    res.sendFile(__dirname + "/results.html");
+    res.render("results")
 });
 
 app.listen(3000, function(){ 
