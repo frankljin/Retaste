368cc71ef61dcd7fc162f47dfe3d847b05199d9c
frankljin <52306663+frankljin@users.noreply.github.com>
2020-12-18 23:18:21 -0700

    API connection + retrieve JSON

--- app.js
+++ app.js
@@ -1,32 +1,51 @@
+// constants for initialization
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
-const api_url = "https://api.spoonacular.com/recipes/findByIngredients";
-const api_key = "5ebf32f8374945ddba4efdce210b90d3";
+const https = require("https");
 const app = express();
 const ejs = require("ejs");
+const api_url = "https://api.spoonacular.com/recipes/findByIngredients";
+const api_key = "5ebf32f8374945ddba4efdce210b90d3";
 
+// access files/locations required for project 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.set('view engine', 'ejs');
 
+// homepage 
 app.get("/", function(req, res){
     res.render("home");
 });
 
+// displays after searching 
 app.get("/results", function(req, res){
     res.render("results")
 });
 
+// startup 
 app.listen(3000, function(){ 
     console.log("Server is running on port 3000");
 });
 
+// take user input
 app.post("/", function(req, res){
-    const ingredients = req.body.ingredients;
+    let ingredients = req.body.ingredients;
+    ingredients = ingredients.replace(/\s+/g, '');
 
-    console.log(ingredients);
+    const url = api_url + "?apiKey=" + api_key + "&ingredients=" + ingredients + "&number=6";
+
+    https.get(url, function(response){
+        let data = "";
+        response.on("data", (chunk) => {
+            data += chunk;
+        });
+        response.on("end", () => {
+            const body = JSON.parse(data);
+            console.log(body);
+        })
+    });
+
 
     res.redirect("/results");
 });
