297ba568c55a105f5c956060058dab1bad453cf8
frankljin <52306663+frankljin@users.noreply.github.com>
2021-01-06 22:48:46 -0700

    Trying to add links
    but i ran out of api calls for the day XD

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 const ejs = require("ejs");
 const api_url = "https://api.spoonacular.com/recipes/findByIngredients";
 const api_key = "5ebf32f8374945ddba4efdce210b90d3";
+const recipe_link = "https://api.spoonacular.com/recipes/";
 
 // access files/locations required for project 
 app.use(express.static('public'));
@@ -42,26 +43,52 @@ app.post("/", function(req, res){
         response.on("data", (chunk) => {
             data += chunk;
         });
+        
+        // Initialize empty lists to store contents
         response.on("end", () => {
             const apibody = JSON.parse(data);
             var namelist = [];
             var imglist = [];
             var inglist = [];
+            var jsonlist = [];
+            var linklist = [];
 
+            // Push contents from JSON into lists
             apibody.forEach(function(m){
                 namelist.push(m[Object.keys(m)[1]]);
                 imglist.push(m[Object.keys(m)[2]]);
                 inglist.push(m[Object.keys(m)[4]]);
+                jsonlist.push(recipe_link + m[Object.keys(m)[0]].toString() + "/information/?apiKey=" + api_key + "&includeNutrition=false");
             })
-
+            
+            jsonlist.forEach(function(m){
+                https.get(m, function(response){
+                    
+                    // JSON is sent in chunks... make sure all is sent
+                    let data = "";
+                    response.on("data", (chunk) => {
+                        data += chunk;
+                    });
+                    
+                    response.on("end", () => {
+                        const apibody2 = JSON.parse(data);
+                    
+                        linklist.push(apibody2.sourceUrl);
+                        console.log(apibody2.sourceUrl);
+                        console.log(linklist);
+                    })
+                    
+                })
+            })
+            
+            
+            // send to results.ejs
             res.render("results", {
                 names: namelist,
                 images: imglist,
-                ingredients: inglist
+                ingredients: inglist,
+                link: linklist
             })
         })
-    });
-
-    
+    }); 
 });
-
